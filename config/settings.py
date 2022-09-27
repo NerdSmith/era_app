bbc3d2354f24849212384a134a13557f908dc70f
Nikita Merzlyakov <merzlyakov02@gmail.com>
2022-09-28 01:28:55 +0300

    cors allowed origins delete

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ DEBUG = int(os.environ.get("DEBUG", default=1))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost 127.0.0.1 [::1]").split(" ")
 
-CORS_ALLOWED_ORIGINS = os.environ.get("DJANGO_ALLOWED_HOSTS", "http://localhost http://127.0.0.1").split(" ")
+# CORS_ALLOWED_ORIGINS = os.environ.get("DJANGO_ALLOWED_HOSTS", "http://localhost http://127.0.0.1").split(" ")
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = False
